c853e5883025f868a12f418bd5cdf57a8e2affe1
Caio Vieira Fernandes <caiovieiraprofissional@proton.me>
2025-12-04 21:43:02 -0300

    updated some configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -28,3 +28,38 @@ end, { desc = '[C]argo [R]un' })
 
 -- Telescope
 vim.keymap.set('n', '<space>df', require('telescope.builtin').find_files, { desc = 'Find project files' })
+
+-- Fold scopes
+-- local vim = vim
+-- local opt = vim.opt
+--
+-- opt.foldmethod = 'expr'
+-- opt.foldexpr = 'nvim_treesitter#foldexpr()'
+--
+-- -- Folding defaults to fold everything when you
+-- -- open a new file, so we need to unfold it
+-- local api = vim.api
+-- local M = {}
+--
+-- -- function to create a list of commands and convert them to autocommands
+-- -------- This function is taken from https://github.com/norcalli/nvim_utils
+-- function M.nvim_create_augroups(definitions)
+--   for group_name, definition in pairs(definitions) do
+--     api.nvim_command('augroup ' .. group_name)
+--     api.nvim_command 'autocmd!'
+--     for _, def in ipairs(definition) do
+--       local command = table.concat(vim.tbl_flatten { 'autocmd', def }, ' ')
+--       api.nvim_command(command)
+--     end
+--     api.nvim_command 'augroup END'
+--   end
+-- end
+--
+-- local autoCommands = {
+--   -- other autocommands
+--   open_folds = {
+--     { 'BufReadPost,FileReadPost', '*', 'normal zR' },
+--   },
+-- }
+--
+-- M.nvim_create_augroups(autoCommands)
--- nvim/.config/nvim/lua/kickstart/default.lua
+++ nvim/.config/nvim/lua/kickstart/default.lua
@@ -146,6 +146,9 @@ vim.opt.splitbelow = true
 --  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
@@ -239,7 +242,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- Zig LSP
   'ziglang/zig.vim',
@@ -667,9 +670,10 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        -- gopls = {},
+        gopls = {},
         pyright = {},
         rust_analyzer = {},
+
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -881,28 +885,6 @@ require('lazy').setup({
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
     },
-  },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rebelot/kanagawa.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('kanagawa').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa-dragon'
-    end,
   },
 
   -- Highlight todo, notes, etc in comments
